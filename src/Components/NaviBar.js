6802070233dc79d4b713084af0e3c393b9a0be74
ihor.osypov <ihor.osypov@geeksforless.com>
2021-02-11 18:08:11 +0200

    Added modal for sign in

--- src/Components/NaviBar.js
+++ src/Components/NaviBar.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Nav, Navbar, Button, Container } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Nav, Navbar, Button, Container, Modal, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import styled from "styled-components";
 
@@ -16,6 +16,12 @@ const Styles = styled.div `
 `
 
 export default function NaviBar(){
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return(
         <>
             <Styles>
@@ -30,13 +36,34 @@ export default function NaviBar(){
                                 <Nav.Link><Link to="/about">About</Link></Nav.Link>
                             </Nav>
                             <Nav>
-                                <Button variant="primary" className="mr-2">Log In</Button>
-                                <Button variant="primary">Sign Out</Button>
+                                <Button variant="primary" className="mr-2" onClick={handleShow}>Log In</Button>
+                                <Button variant="primary" onClick={handleShow}>Sign Out</Button>
                             </Nav>
                         </Navbar.Collapse>
                     </Container>
                 </Navbar>
             </Styles>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Log in</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="email" placeholder="Enter Email"/>
+                            <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
+                        </Form.Group>
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Enter Password"/>
+                        </Form.Group>
+                        <Form.Group controlId="formBasicCheckbox">
+                            <Form.Check type="checkbox" label="Remember me"/>
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+            </Modal>
         </>
     )
 }
